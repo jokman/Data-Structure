92015d25d4fc61709dcde2ef22786e33df567f3a
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-01 02:02:33 -0300

    creating initialize and free functions of tree

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#define ERROR -1;
+
 
 using namespace std;
 
@@ -6,12 +8,49 @@ typedef struct tree *Tree;
 
 /* Structs */
 struct tree {
-
   int info;
   struct tree *left;
   struct tree *right;
-
 };
+
+/* Functions */
+Tree* initialize_tree() {
+
+  Tree *root = (Tree*)malloc(sizof(Tree));
+
+  /* Here I verify, if tree was successful allocated */
+  if(root != NULL) {
+    *root = NULL;
+  } else {
+    cout << "Something went wrong, tree was not initialize\n";
+    system.exit(ERROR);
+  }
+  return root;
+}
+
+void free_node(Tree node) {
+
+  if(node == NULL) {
+    /* Nothing to do */
+    return;
+  } else {
+    free_node(node->left);
+    free_node(node->right);
+    free(node);
+    node = NULL;
+  }
+}
+
+void free_tree(Tree *root) {
+
+  if(root == NULL) {
+    /* Nothing to do */
+    return;
+  } else {
+    free_node(*root);
+    free(root);
+  }
+}
 
 int main() {
 
